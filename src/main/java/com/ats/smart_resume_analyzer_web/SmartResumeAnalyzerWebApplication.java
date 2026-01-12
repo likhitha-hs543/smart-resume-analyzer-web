880e9b01e29418b83a104bf12b66db0e191e0493
likhitha-hs543 <likhithahs543@gmail.com>
2026-01-12 15:48:54 +0530

    Add scanBasePackages to fix controller registration

--- src/main/java/com/ats/smart_resume_analyzer_web/SmartResumeAnalyzerWebApplication.java
+++ src/main/java/com/ats/smart_resume_analyzer_web/SmartResumeAnalyzerWebApplication.java
@@ -3,7 +3,7 @@ package com.ats.smart_resume_analyzer_web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.ats")
 public class SmartResumeAnalyzerWebApplication {
 
 	public static void main(String[] args) {
