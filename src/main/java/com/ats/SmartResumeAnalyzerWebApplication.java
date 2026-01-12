4a0aa9601aa422b2db363b8400578be5b8932925
likhitha-hs543 <likhithahs543@gmail.com>
2026-01-12 15:54:22 +0530

    Fix package structure: move main class to com.ats root package

---
+++ src/main/java/com/ats/SmartResumeAnalyzerWebApplication.java
@@ -0,0 +1,13 @@
+package com.ats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartResumeAnalyzerWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartResumeAnalyzerWebApplication.class, args);
+    }
+
+}
