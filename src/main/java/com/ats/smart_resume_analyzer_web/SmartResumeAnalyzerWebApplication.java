86a13008eeb73d496333f0554843dffed92a51c8
likhitha-hs543 <likhithahs543@gmail.com>
2026-01-12 10:08:33 +0530

    Initialize Spring Boot project for Smart Resume Analyzer web app

---
+++ src/main/java/com/ats/smart_resume_analyzer_web/SmartResumeAnalyzerWebApplication.java
@@ -0,0 +1,13 @@
+package com.ats.smart_resume_analyzer_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartResumeAnalyzerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartResumeAnalyzerWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ats/smart_resume_analyzer_web/SmartResumeAnalyzerWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ats.smart_resume_analyzer_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartResumeAnalyzerWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
